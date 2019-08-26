1d1e8b11e4fbd96e76c130d190e4dadf02b20f18
skandergod <thedanidacosta@gmail.com>
2019-08-26 10:26:00 -0400

    route by id implemented

--- routes/profile.js
+++ routes/profile.js
@@ -21,6 +21,22 @@ router.get('/:id', function(req, res, next) {
   
 });
 
+router.get('/getPostsByid', function(req, res, next) {
+  // GET/users/ route
+  connection.query(`SELECT * FROM posts WHERE user_post = ${req.params.id}
+  ORDER BY posts.date DESC
+  LIMIT ${req.body.from}, ${req.body.to}
+  `,function(err,rows){
+    if(err){
+      return res.status(203).json({valid:false, error: 'Error'})   
+    }else{
+      console.log(rows);
+      return res.json({valid:true, result: rows});
+    }                   
+  });
+});
+
+
 
 module.exports = router;
 
