604764ea268e5722da5228567bd891dd1d766d99
skandergod <thedanidacosta@gmail.com>
2019-08-26 17:37:24 -0400

    agregado insert en likes

--- routes/likes.js
+++ routes/likes.js
@@ -6,6 +6,28 @@ var connection  = require('../config/db');
 var bcrypt = require('bcrypt');
 import multer from 'multer';
 
+router.post("/createLike", function(req, res){
 
+    var values = '', indexs = '';
+    let i = 0;
+    for(var key in req.body){
+        if(key == 'text' /*|| key == 'media'*/){values += "'"+req.body[key]+"'"; indexs+= key;}
+        else{values += req.body[key]; indexs+= key;}
+        if(i != Object.keys(req.body).length-1){values += ', '; indexs+= ', ';}
+        i++
+    };
+    
+    connection.query(`
+        INSERT INTO likes
+        (${indexs})
+        VALUES (${values})  
+    `,function(err,rows){
+        console.log(err)
+        if(err) return res.json({valid:false, error:'Error'})
+        if(rows) return res.json({
+            valid: true
+        })
+    })
+})
 
 module.exports = router;
