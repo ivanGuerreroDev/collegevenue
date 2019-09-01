fd9209d4c920487c6f391674ee4cd047629cb213
Ivan Guerrero <guerrero.ivan.789@gmai.com>
2019-09-01 12:26:56 -0500

    1.9.2019.12.27

--- routes/posts.js
+++ routes/posts.js
@@ -107,12 +107,8 @@ router.post('/getTrendingPosts', function(req, res, next) {
     FROM posts 
     INNER JOIN users ON posts.user_post = users.id 
     ORDER BY posts.shares DESC, posts.likes DESC, posts.comments DESC
-<<<<<<< HEAD
+    WHERE posts.date <= ${req.body.dateFrom} AND posts.date > ${req.body.dateTo}
     LIMIT ${req.body.from},${req.body.to}
-=======
-    WHERE posts.date <= ${req.date.from} AND posts.date > ${req.date.to}
-    LIMIT ${req.from},${req.to}
->>>>>>> 74cbe82d9a975d9f54215019a398997073b62200
     `,function(err,rows){
       if(err){
         console.log(err)
