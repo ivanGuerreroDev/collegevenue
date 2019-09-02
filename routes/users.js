a99b2f26eb9c6c6a0a3167f417ed55690134ab89
skandergod <thedanidacosta@gmail.com>
2019-09-01 22:56:17 -0400

    forgot password implementado

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,9 @@ const path = require('path');
 var passport = require('passport')
 var connection  = require('../config/db');
 var bcrypt = require('bcrypt');
+var nodeMailer = require('nodemailer');
+
+
 
 router.get('/', function(req, res, next) {
   res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
@@ -147,10 +150,134 @@ router.post('/user/create', isLoggedIn, function(req, res, next) {
   });
   
 });
+
+router.post('/recovery', function(req,res) {
+
+  connection.query(`
+  SELECT *
+  FROM password_request
+  WHERE email = '${req.body.email}' AND email IN (SELECT correo
+                                                FROM users
+                                                WHERE correo = '${req.body.email}')
+  `,function(err,rows){
+    if(err) {
+      console.log(err);
+      console.log('Error en Request 1');
+      return res.json({valid:false, notice: 'Error on Request'})
+    }else{
+    if(rows.length > 0){
+      console.log(rows);
+        connection.query(`
+        UPDATE password_request
+        SET valid = 1
+        WHERE email = '${req.body.email}'
+        `,function(err,rows){
+          if(err){
+            console.log(err);
+            console.log('Error en Request 2');
+            return res.json({valid:false, notice: 'Error on Request'})
+          }else{
+            console.log('Token Valido');
+            return res.json({valid:true, notice: 'Please Insert Your new password!'})
+          }
+        });
+      }else{
+        return res.json({valid:false, notice: 'That account did not ask for a code'});
+      }       
+    }      
+  })
+  
+  
+})
+
+router.post('/newPassword', function(req,res) {
+
+  connection.query(`
+  SELECT *
+  FROM password_request
+  WHERE email = '${req.body.email}' AND email IN (SELECT correo
+                                                FROM users
+                                                WHERE correo = '${req.body.email}')
+  AND valid = 1
+  AND token = '${req.body.token}'
+  `,function(err,rows){
+    if(err) {
+      console.log(err);
+      console.log('Error en Request 1');
+      return res.json({valid:false, notice: 'Your Token is invalid or this account did not ask for a code yet'})
+    }else{
+    if(rows){
+      req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null);
+      console.log(rows);
+        connection.query(`
+        UPDATE users
+        SET password = '${req.body.password}'
+        WHERE correo = '${req.body.email}'
+        `,function(err,rows){
+          if(err){
+            console.log(err);
+            console.log('Error en Request 2');
+            return res.json({valid:false, notice: 'Error on Request'})
+          }else{
+            console.log('Password Cambiada!');
+            return res.json({valid:true, notice: 'Your new password is saved, now try to log in!'})
+          }
+        });
+      }else{
+        return res.json({valid:false, notice: 'That account does not exists'})
+      }       
+    }      
+  })
+
+})
+
+router.post('/forgotPassword', function(req,res) {
+
+  var code = makeid(8);
+
+  connection.query(`
+  SELECT *
+  FROM password_request
+  WHERE email = '${req.body.email}' AND email IN (SELECT correo
+                                                FROM users
+                                                WHERE correo = '${req.body.email}')
+  `,function(err,rows){
+    if(err) {
+      console.log(err);
+      console.log('Error en Request 1');
+      return res.json({valid:false, notice: 'Error on Request'})
+    }else{
+    if(rows.length > 0){
+      console.log(rows);
+      console.log('Ese Email ya tiene un Codigo');
+      return res.json({valid:false, notice: 'That account already requested a code'})
+      }else{
+        sendCode(req.body.email,code);
+        connection.query(`
+        INSERT INTO password_request (email,token,valid)
+        VALUES ('${req.body.email}',${code},0)
+        `,function(err,rows){
+          if(err){
+            console.log(err);
+            console.log('Error en Request 2');
+            return res.json({valid:false, notice: 'Error on Request'})
+          }else{
+            console.log('codigo enviado al email');
+            return res.json({valid:true, notice: 'The code has been sent to your email!'})
+          }
+        });
+      }       
+    }      
+  })
+
+})
+
 router.get('/logout', function(req, res) {
   req.logout();
   res.redirect('/');
 });
+
+
 
 module.exports = router;
 
@@ -159,3 +286,38 @@ function isLoggedIn(req, res, next) {
 		return next();
 	res.redirect('/');
 }
+
+function makeid(length) {
+  console.log('ejecutado makeid')
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+function sendCode(email, code){
+  console.log('ejecutado sendCode')
+  let transporter = nodeMailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'noreplybusient@gmail.com',
+      pass: 'BuSiNeT1'
+    }
+  });
+  let mailOptions = {
+    from: '"Businet" <noreplybusient@gmail.com>', // sender address
+    to: email, // list of receivers
+    subject: "Verification Code from Businet", // Subject line
+    text: "Verification Code", // plain text body
+    html: '<b>The Verification Code is: '+code+'</b>' // html body
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) return console.log(error);
+    console.log('Message %s sent: %s', info.messageId, info.response);
+  });
+};
